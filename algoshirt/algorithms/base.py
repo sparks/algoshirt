cfe9b50a53557931121ab789803ce41cc5a3c8ec
Xavier Snelgrove <xavier@cs.toronto.edu>
2013-10-19 01:07:27 -0400

    Added topo algorithm

---
+++ algoshirt/algorithms/base.py
@@ -0,0 +1,19 @@
+import cairo
+
+class BaseRenderer(object):
+    def __init__(self, params):
+        self.params = params
+
+    def render_to_surface(self, surface, w, h):
+        raise NotImplemented("Children must implement render_to_surface")
+
+    def render_to_svg(self, filename, w, h):
+        surf = cairo.SVGSurface(filename, w, h)
+        self.render_to_surface(surf, w, h)
+        surf.finish()
+
+    def render_to_png(self, filename, w, h):
+        surf = cairo.ImageSurface(cairo.FORMAT_ARGB32, w, h)
+        self.render_to_surface(surf, w, h)
+        surf.write_to_png(filename)
+
